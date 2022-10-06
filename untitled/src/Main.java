28831d9ad685646290174a0ec01656980233c1db
Per50nA <awm02102020@mail.ru>
2022-10-06 14:51:57 +0300

    3 ver

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -49,8 +49,8 @@ public class Main {
         boolean n2 = false;
         try {
             if (isNum(num1)  && isNum(num2) ){//проверяем систему счисления
-                if (Integer.parseInt(num1) >= 10 && Integer.parseInt(num2) >= 10) {
-                    throw new Exception("number is over 10");
+                if (Integer.parseInt(num1) > 10 || Integer.parseInt(num1) < 1  || (Integer.parseInt(num2)) > 10 || Integer.parseInt(num2) < 1) {
+                    throw new Exception("number is over 10 or lower 1");
                 }
             }else {
                 for (int i=0;i< rim.length;i++){//перевод из римских в арабские
